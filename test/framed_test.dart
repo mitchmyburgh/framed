5de643e982d77460724e8ee007f2dfa7aa2842f0
Mitch Myburgh <mitchmyburgh@gmail.com>
2018-09-08 18:46:27 +0200

    initial commit

---
+++ test/framed_test.dart
@@ -0,0 +1,16 @@
+import 'package:framed/framed.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    Awesome awesome;
+
+    setUp(() {
+      awesome = new Awesome();
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
